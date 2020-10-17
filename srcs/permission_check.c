19e5a97ec252e34046288f070917f34646a406a8
Fitz-commits <christopher.delaby@hotmail.com>
2020-10-17 10:07:57 +0200

    permission.c

---
+++ srcs/permission_check.c
@@ -0,0 +1,34 @@
+#include "minishell.h"
+
+/*
+**	I need a function that deals with file
+**		file is for path and redirection
+**
+**	I need another one that deals with directory
+**	directory is for cd 
+**
+**
+**
+**
+*/
+
+/*int		check_fperm(t_mshl *m, char *path, int fdflag)
+{
+	struct stat buffer;
+	return (EXIT_SUCCESS);
+}*/
+
+int		check_dperm(t_mshl *m, char *path)
+{
+	struct stat buffer;
+	mode_t perm;
+	
+	if ((stat(path, &buffer) == -1))
+		return (m->ierr = m->begin);
+	perm = buffer.st_mode;
+	if (!S_ISDIR(buffer.st_mode))
+		return ((m->err = 6) && (m->ierr = m->progr)); //is not a dir
+	if ((!(perm & S_IRWXU)) && !(perm &  S_IXUSR))
+		return ((m->err = 7) && (m->ierr = m->progr)); //not authorized	
+	return (EXIT_SUCCESS);
+}
