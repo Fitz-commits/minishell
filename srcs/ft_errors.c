db027a389bc84d3fc15e27ed5021b185352fb18b
MrSchnapps <jules.decuypere@hotmail.fr>
2020-10-18 16:02:51 +0200

    remise sterror

--- srcs/ft_errors.c
+++ srcs/ft_errors.c
@@ -2,11 +2,11 @@
 
 void    print_errno(char *str)
 {
-    ft_putstr_fd("minishell : ", 2);
+    ft_putstr_fd("minishell: ", 2);
     if (str)
     {
         ft_putstr_fd(str, 2);
-        ft_putstr_fd(" : ", 2);
+        ft_putstr_fd(": ", 2);
     }
     ft_putendl_fd(strerror(errno), 2);
 }
